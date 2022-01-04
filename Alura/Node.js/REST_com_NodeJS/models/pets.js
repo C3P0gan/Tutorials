9c82402f6eb3580c33842e2bb6046d27419aa0ba
C3P0gan <charles_mendes@econeteditora.com.br>
2022-01-04 11:21:47 -0300

    modified: Conclusão NodeJS: Streaming de dados e Repositório

--- Alura/Node.js/REST_com_NodeJS/models/pets.js
+++ Alura/Node.js/REST_com_NodeJS/models/pets.js
@@ -1,5 +1,5 @@
-const conexao = require('../infraestrutura/conexao')
-const uploadDeArquivo = require('../arquivos/uploadFiles')
+const conexao = require('../infraestrutura/database/conexao')
+const uploadDeArquivo = require('../infraestrutura/arquivos/uploadFiles')
 
 class Pet {
     adiciona(pet, res) {
