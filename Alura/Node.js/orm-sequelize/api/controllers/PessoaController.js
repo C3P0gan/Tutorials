efddc09befa0af26db7d08458dec5669990920c9
C3P0gan <charles_mendes@econeteditora.com.br>
2022-01-06 14:50:10 -0300

    modified: ORM com NodeJS: API com Sequelize e MySQL [04. CRUD com Sequelize]

--- Alura/Node.js/orm-sequelize/api/controllers/PessoaController.js
+++ Alura/Node.js/orm-sequelize/api/controllers/PessoaController.js
@@ -1,13 +1,94 @@
 const database = require('../models')
 
 class PessoaController {
-    // Read
+    // Read all
     static async retornaTodasAsPessoas(req, res) {
-        try{
+        try {
             const todasAsPessoas = await database.Pessoas.findAll()
             return res
                 .status(200) // OK
                 .json(todasAsPessoas)
+        } catch (error) {
+            return res
+                .status(500) // Internal Server Error
+                .json(error.message)
+        }
+    }
+
+    // Read one
+    static async retornaUmaPessoa(req, res) {
+        const { id } = req.params
+        try {
+            const umaPessoa = await database.Pessoas.findOne({
+                where: {
+                    id: Number(id)
+                }
+            })
+            return res
+                .status(200) // OK
+                .json(umaPessoa)
+        } catch (error) {
+            return res
+                .status(500) // Internal Server Error
+                .json(error.message)
+        }
+    }
+
+    // Create
+    static async criaPessoa(req, res) {
+        const novaPessoa = req.body
+        try {
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
+            return res
+                .status(201) // Created
+                .json(novaPessoaCriada)
+        } catch (error) {
+            return res
+                .status(500) // Internal Server Error
+                .json(error.message)
+        }
+    }
+
+    // Update
+    static async atualizaPessoa(req, res) {
+        const { id } = req.params
+        const novasInfos = req.body
+        try {
+            await database.Pessoas.update(novasInfos, {
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            const pessoaAtualizada = await database.Pessoas.findOne({
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            return res
+                .status(200) // OK
+                .json(pessoaAtualizada)
+        } catch (error) {
+            return res
+                .status(500) // Internal Server Error
+                .json(error.message)
+        }
+    }
+
+    // Delete
+    static async apagaPessoa(req, res) {
+        const { id } = req.params
+        try {
+            await database.Pessoas.destroy({
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            return res
+                .status(204) // No Content
+                .end()
         } catch (error) {
             return res
                 .status(500) // Internal Server Error
--- Alura/Node.js/orm-sequelize/api/routes/pessoasRoute.js
+++ Alura/Node.js/orm-sequelize/api/routes/pessoasRoute.js
@@ -4,6 +4,11 @@ const PessoaController = require('../controllers/PessoaController')
 
 const router = Router()
 
-router.get('/pessoas', PessoaController.retornaTodasAsPessoas)
+// CRUD Pessoas
+router.post('/pessoas', PessoaController.criaPessoa) // Create
+router.get('/pessoas', PessoaController.retornaTodasAsPessoas) // Read all
+router.get('/pessoas/:id', PessoaController.retornaUmaPessoa) // Read one
+router.put('/pessoas/:id', PessoaController.atualizaPessoa) // Update
+router.delete('/pessoas/:id', PessoaController.apagaPessoa) // Delete
 
 module.exports = router
