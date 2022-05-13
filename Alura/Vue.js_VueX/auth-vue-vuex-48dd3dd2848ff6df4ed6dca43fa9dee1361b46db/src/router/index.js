d907f779e466ba46dea22277e90966a5765fe885
Cristoffer Mario Pogan <cristoffer_pogan@econeteditora.com.br>
2022-05-13 17:30:40 -0300

    updated: Login & Logout

--- Alura/Vue.js_VueX/auth-vue-vuex-48dd3dd2848ff6df4ed6dca43fa9dee1361b46db/src/router/index.js
+++ Alura/Vue.js_VueX/auth-vue-vuex-48dd3dd2848ff6df4ed6dca43fa9dee1361b46db/src/router/index.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Gerentes from '../views/Gerentes.vue'
-import Home from '../views/Home.vue'
-import NovoUsuario from '../views/NovoUsuario.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Gerentes from '../views/Gerentes.vue';
+import Home from '../views/Home.vue';
+import NovoUsuario from '../views/NovoUsuario.vue';
+import Login from '../views/Login.vue';
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
     path: '/cadastre-se',
     name: 'novo.usuario',
     component: NovoUsuario
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   }
 ]
 
