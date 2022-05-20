ecfaf69cf221ab6f529547e1e4d98b4541198170
Cristoffer Mario Pogan <cristoffer_pogan@econeteditora.com.br>
2022-05-20 15:04:05 -0300

    updated: 03. Refatorando e evoluindo o código

--- Alura/Vue.js_VueX/auth-vue-vuex-48dd3dd2848ff6df4ed6dca43fa9dee1361b46db/src/provedor.js
+++ Alura/Vue.js_VueX/auth-vue-vuex-48dd3dd2848ff6df4ed6dca43fa9dee1361b46db/src/provedor.js
@@ -1,5 +1,6 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
+import http from '@/http';
 
 Vue.use(Vuex)
 
@@ -8,6 +9,38 @@ const estado = {
     usuario: {}
 };
 
+const mutations = {
+    DEFINIR_USUARIO_LOGADO (state, { token, usuario }) {
+        state.usuario = usuario,
+        state.token = token
+    },
+    DESLOGAR_USUARIO (state) {
+        state.token = null
+        state.usuario = {}
+    }
+};
+
+const actions = {
+    efetuarLogin ({ commit }, usuario) {
+        return new Promise((resolve, reject) => {
+            http.post('auth/login', usuario)
+                .then(res => {
+                    commit('DEFINIR_USUARIO_LOGADO', {
+                        token: res.data.access_token,
+                        usuario: res.data.user
+                    })
+                    resolve(res.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                    reject(err);
+                });
+        });
+    }
+};
+
 export default new Vuex.Store({
-    state: estado
-})
+    state: estado,
+    mutations,
+    actions
+});
--- Alura/Vue.js_VueX/auth-vue-vuex-48dd3dd2848ff6df4ed6dca43fa9dee1361b46db/src/views/Login.vue
+++ Alura/Vue.js_VueX/auth-vue-vuex-48dd3dd2848ff6df4ed6dca43fa9dee1361b46db/src/views/Login.vue
@@ -28,15 +28,8 @@ export default {
     },
     methods: {
         efetuarLogin () {
-            this.$http.post('auth/login', this.usuario)
-                .then(res => {
-                    console.log(res)
-                    // localStorage.setItem('token', res.data.access_token)
-                    this.$store.state.token = res.data.access_token
-                    this.$store.state.usuario = res.data.user
-                    this.$router.push({ name: 'gerentes' })
-                })
-                .catch(err => console.log(err));
+            this.$store.dispatch('efetuarLogin', this.usuario)
+                .then(() => this.$router.push({ name: 'gerentes' }));
         }
     }
 };
