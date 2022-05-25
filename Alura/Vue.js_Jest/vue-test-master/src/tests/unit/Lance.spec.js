e01375166630980f530dd9d10f391a5228213ca0
Cristoffer Mario Pogan <cristoffer_pogan@econeteditora.com.br>
2022-05-25 10:06:28 -0300

    finished: 02. Primeiros passos

--- Alura/Vue.js_Jest/vue-test-master/src/tests/unit/Lance.spec.js
+++ Alura/Vue.js_Jest/vue-test-master/src/tests/unit/Lance.spec.js
@@ -3,5 +3,28 @@ import { mount } from '@vue/test-utils';
 
 test('Não aceita lances com valor inferior a zero.', () => {
     const wrapper = mount(Lance);
-    expect(wrapper).toBeTruthy();
+    const input = wrapper.find('input');
+    input.setValue(-100);
+    const lancesEmitidos = wrapper.emitted('novo-lance');
+    wrapper.trigger('submit');
+    expect(lancesEmitidos).toBeUndefined();
 });
+
+test('Emite um lance quando o valor é superior a zero.', () => {
+    const wrapper = mount(Lance);
+    const input = wrapper.find('input');
+    input.setValue(100);
+    wrapper.trigger('submit');
+    const lancesEmitidos = wrapper.emitted('novo-lance');
+    expect(lancesEmitidos).toHaveLength(1);
+});
+
+test('Emite o valor esperado de um lance válido.', () => {
+    const wrapper = mount(Lance);
+    const input = wrapper.find('input');
+    input.setValue(100);
+    wrapper.trigger('submit');
+    const lancesEmitidos = wrapper.emitted('novo-lance');
+    const lance = parseInt(lancesEmitidos[0][0]);
+    expect(lance).toBe(100);
+})
