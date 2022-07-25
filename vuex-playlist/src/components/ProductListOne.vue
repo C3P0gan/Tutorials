109ced4dc8f033a293f11daf3d88cd1860043348
Cristoffer Mario Pogan <cristoffer_pogan@econeteditora.com.br>
2022-07-25 17:30:32 -0300

    VueX Tutorial

---
+++ vuex-playlist/src/components/ProductListOne.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="product-list-one">
+    <h2>Product List One</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
