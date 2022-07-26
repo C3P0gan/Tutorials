f9bf366b1aacaacc2aa792f656f7003f7f13e62f
Cristoffer Mario Pogan <cristoffer_pogan@econeteditora.com.br>
2022-07-26 10:11:43 -0300

    VueX this..does.not.work

--- vuex-playlist/src/App.vue
+++ vuex-playlist/src/App.vue
@@ -1,33 +1,29 @@
 <template>
   <div id="app">
-    <product-list-one v-bind:products="products"></product-list-one>
-    <product-list-two v-bind:products="products"></product-list-two>
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
   </div>
 </template>
 
 <script>
-import ProductListOne from './components/ProductListOne.vue';
-import ProductListTwo from './components/ProductListTwo.vue';
+import ProductListOne from "./components/ProductListOne.vue";
+import ProductListTwo from "./components/ProductListTwo.vue";
 
 export default {
   components: {
-    'product-list-one': ProductListOne,
-    'product-list-two': ProductListTwo,
+    "product-list-one": ProductListOne,
+    "product-list-two": ProductListTwo,
   },
-  name: 'app',
-  data () {
-    return {
-      products: [
-        {name: 'Banana Skin', price: 20},
-        {name: 'Shiny Star', price: 40},
-        {name: 'Green Shells', price: 60},
-        {name: 'Red Shells', price: 80}
-      ]
-    }
-  }
-}
+  name: "app",
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
-
+body {
+  font-family: Ubuntu;
+  color: #555;
+}
 </style>
