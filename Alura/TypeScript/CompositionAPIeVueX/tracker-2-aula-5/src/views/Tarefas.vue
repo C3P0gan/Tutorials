b0ae50463c97bea37336661d4e0ba9d666873820
Cristoffer Mario Pogan <cristoffer_pogan@econeteditora.com.br>
2022-07-27 17:31:44 -0300

    finalizado: Aula 04 Atividades 03

--- Alura/TypeScript/CompositionAPIeVueX/tracker-2-aula-5/src/views/Tarefas.vue
+++ Alura/TypeScript/CompositionAPIeVueX/tracker-2-aula-5/src/views/Tarefas.vue
@@ -6,6 +6,19 @@
         Você não está muito produtivo hoje
         <span class="has-text-weight-bold">:(</span>
       </Box>
+      <div class="field">
+        <p class="control has-icons-left">
+          <input
+            class="input"
+            type="text"
+            placeholder="Digite para filtrar"
+            v-model="filtro"
+          />
+          <span class="icon is-small is-left">
+            <i class="fas fa-search"></i>
+          </span>
+        </p>
+      </div>
       <Tarefa
         v-for="(tarefa, index) in tarefas"
         :tarefa="tarefa"
@@ -51,7 +64,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, ref, watchEffect } from "vue";
 import Formulario from "../components/Formulario.vue";
 import Tarefa from "../components/Tarefa.vue";
 import Box from "../components/Box.vue";
@@ -94,7 +107,8 @@ export default defineComponent({
   },
   computed: {
     semTarefas(): boolean {
-      return this.tarefas.length == 0;
+      let response = this.tarefas ? this.tarefas.length == 0 : false;
+      return response;
     },
   },
   setup() {
@@ -102,9 +116,22 @@ export default defineComponent({
     store.dispatch(OBTER_TAREFAS);
     store.dispatch(OBTER_PROJETOS);
 
+    const filtro = ref("");
+
+    // const tarefas = computed(() =>
+    //   store.state.tarefa.tarefas.filter(
+    //     (t) => !filtro.value || t.descricao.includes(filtro.value)
+    //   )
+    // );
+
+    watchEffect(() => {
+      store.dispatch(OBTER_TAREFAS, filtro.value);
+    });
+
     return {
-      tarefas: computed(() => store.state.tarefas),
+      tarefas: computed(() => store.state.tarefa.tarefas),
       store,
+      filtro,
     };
   },
 });
