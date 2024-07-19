7ec560b476dfaed50014cef34f45b153f739d32f
Cristoffer Pogan <cristoffer_pogan@DSN-1003.econet.matriz.org>
2024-07-19 15:56:34 -0300

    [UPDATE] utils.py

--- Alura/Formacao_Machine_Learning_Avancada/Linguagem_Natural/Word2Vec_Treinamento_Word_Embedding/utils.py
+++ Alura/Formacao_Machine_Learning_Avancada/Linguagem_Natural/Word2Vec_Treinamento_Word_Embedding/utils.py
@@ -2,20 +2,23 @@
 import spacy
 import numpy as np
 
-nlp = spacy.load("pt_core_news_sm", disable=["parser", "ner", "tagger", "textcat"])
+nlp = spacy.load("pt_core_news_sm", disable=[
+                 "parser", "ner", "tagger", "textcat"])
+
 
 def tokenizador(texto):
     doc = nlp(texto)
     tokens_validos = []
 
     for token in doc:
-       
+
         e_valido = not token.is_stop and token.is_alpha
 
         if e_valido:
             tokens_validos.append(token.text.lower())
 
     return tokens_validos
+
 
 def combinacao_de_vetores_por_soma(palavras, modelo):
 
@@ -29,4 +32,3 @@ def combinacao_de_vetores_por_soma(palavras, modelo):
             pass
 
     return vetor_resultante
-
